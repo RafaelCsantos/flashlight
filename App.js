e882dd3ec3a36f361b86f08a5a6c8060accd380e
Rafael <rafaelcssantos@hotmail.com>
2022-06-17 09:58:54 -0300

    Style

---
+++ App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+const App = () => {
+  
+  const toggle= true; //false
+
+  // if toggle return light
+  return(
+   <View style= {toggle ? style.containerLight : style.container}>
+    <TouchableOpacity onpress= {() => {} }>
+
+
+    <Image style= {toggle ? style.lightiningOn : style.lightiningOff }  
+    source={
+      toggle?require('./assets/icons/eco-light.png')
+      :
+      require('./assets/icons/eco-light-off.png')} 
+      />
+
+<Image style= {style.dioLogo }  
+    source={
+      toggle?require('./assets/icons/logo-dio.png')
+      :
+      require('./assets/icons/logo-dio-white.png')} 
+      />
+
+      </TouchableOpacity>
+       </View>
+       );
+};
+
+export default App;
+
+
+
+const style = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: 'black',
+alignItems: 'center',
+justifyContent: 'center',
+},
+
+containerLight: {
+  flex: 1,
+  backgroundColor: 'white',
+  alignItems: 'center',
+  justifyContent: 'center',
+  },
+
+lightiningOn: {
+ resizeMode: 'contain',
+ alignSelf: 'center',
+ width: 150,
+ height: 150,
+},
+lightiningOff: {
+  resizeMode: 'contain',
+  alignSelf: 'center',
+  width: 150,
+  height: 150,
+  tintColor: 'white',
+ },
+
+
+dioLogo: {
+   resizeMode: 'contain',
+   alignSelf: 'center',
+   width: 250,
+   height: 250,
+  },
+ 
+
+});
